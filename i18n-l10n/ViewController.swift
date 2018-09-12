356b2cfa7595e98d53a6b6fc41be7a389497d605
Vilar da Camara Neto <vilarneto@gmail.com>
2018-09-12 15:56:29 -0400

    Initial Commit

---
+++ i18n-l10n/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  i18n-l10n
+//
+//  Created by Vilar da Camara Neto on 12/09/18.
+//  Copyright © 2018 Vilar da Camara Neto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
